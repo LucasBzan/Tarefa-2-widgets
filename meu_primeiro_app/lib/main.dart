2a6f3327981cc88b818bc51842917992be96770b
LucasBzan <lucasbzanlorenzi@gmail.com>
2026-08-17 21:29:00 -0300

    passo 5

--- meu_primeiro_app/lib/main.dart
+++ meu_primeiro_app/lib/main.dart
@@ -18,6 +18,15 @@ class _ProfileAppState extends State<ProfileApp> {
   // 1. Controller criado dentro da classe
   final TextEditingController _nameController = TextEditingController();
 
+  // Função que atualiza o nome
+  void _updateName() {
+    // setState() diz ao Flutter: "O estado mudou, por favor, reconstrua a UI!"
+    setState(() {
+      // Lê o texto do controller e atualiza a variável de estado
+      _displayedName = _nameController.text;
+    });
+  }
+
   @override
   void dispose() {
     // Boa prática: descartar o controller quando a tela for destruída
@@ -38,7 +47,6 @@ class _ProfileAppState extends State<ProfileApp> {
           alignment: Alignment.center,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            // 2. Os widgets filhos ficam aqui dentro do Column
             children: [
               Text(
                 _displayedName,
@@ -64,6 +72,14 @@ class _ProfileAppState extends State<ProfileApp> {
                   border: OutlineInputBorder(),
                   labelText: 'Digite seu nome',
                 ),
+              ),
+
+              const SizedBox(height: 20),
+
+              // O botão que aciona a ação
+              ElevatedButton(
+                onPressed: _updateName, // Chama nossa função quando pressionado
+                child: const Text('Atualizar Nome'),
               ),
             ],
           ),
